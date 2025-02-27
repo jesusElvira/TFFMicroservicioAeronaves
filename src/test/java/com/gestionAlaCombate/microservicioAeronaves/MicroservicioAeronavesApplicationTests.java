6b54e5fb5ef43ebd11c73ced17432a76f3418ffe
jesus <jesuselvirasanchez@gmail.com>
2025-02-27 18:36:21 +0100

    Creación MicroservicioAeronaves

---
+++ src/test/java/com/gestionAlaCombate/microservicioAeronaves/MicroservicioAeronavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionAlaCombate.microservicioAeronaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioAeronavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
